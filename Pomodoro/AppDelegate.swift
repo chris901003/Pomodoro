edb22c84cb813400738947df4f9de4d6151da4c3
chris901003 <a0987999103@gmail.com>
2025-05-28 14:06:39 +0800

    Initial Commit

---
+++ Pomodoro/AppDelegate.swift
@@ -0,0 +1,31 @@
+// Created for Pomodoro in 2025
+// Using Swift 6.0
+//
+//
+// Created by HongYan on 2025/5/28.
+// Copyright © 2025 HongYan. All rights reserved.
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
